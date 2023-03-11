360d2545d83928f47f515ee394abdc0ef0e2777c
Gustavo <100693896+gust17x@users.noreply.github.com>
2023-03-10 23:14:37 -0300

    atualizando link da gallery

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,10 +1,9 @@
 // imgs
-import gallery from '../../img/site gallery2.0.png'
+import gallery from '../../img/gallery.png'
 import survivalSland from '../../img/Survival Sland.png'
 import SiteRelogio from '../../img/SITE RELOGIO.png'
 import TaskList from '../../img/TaskList.png'
 import EnvEmail from '../../img/envioEmail.png'
-import provaDev from '../../img/provadev.png'
 import '../Sass/projects.sass'
 
 export const Projects = ({projectPage}) => {
@@ -16,19 +15,7 @@ export const Projects = ({projectPage}) => {
         </nav>
 
         <div className="projects">
-
-        <div className="box">
-            <img src={TaskList} alt="" />
-            <figure className='box-infor'>
-                <nav>
-                <h2 className='infor-title'>TaskList </h2>
-                <p className='infor-text'> <span className='span-infor'>Uma lista de tarefas pra me organizar melhor</span> </p>
-                </nav>
-
-                <a className='btn-to-project' href='https://gust17x.github.io/task-list/'>Ver projeto</a>
-             </figure>
-          </div>
-
+            
           <div className="box">
              <img src={gallery} alt="imagem do site gallery" /> 
              <figure className='box-infor'>
@@ -37,23 +24,10 @@ export const Projects = ({projectPage}) => {
                   <h2 className='infor-title'>Gallery</h2>
                   <p className='infor-text'>Site de galeria simples com display grid</p>
                 </nav>
-                  <a className='btn-to-project' target={'_blank'} href='https://gallery-gold.vercel.app/'>Ver projeto</a>
+                  <a className='btn-to-project' target={'_blank'} href='hhttps://gallery-m.vercel.app/'>Ver projeto</a>
 
              </figure>
           </div>
-          
-          <div className="box">
-            <img src={SiteRelogio} alt="Imagem do site relogio" />
-            <figure className='box-infor'>
-                <nav>
-                <h2 className='infor-title'>Relogio</h2>
-                <p className='infor-text'><span className='span-infor'>Um relogio in live</span></p>
-                </nav>
-
-                <a className='btn-to-project' href='https://gust17x.github.io/Relogio/'>Ver projeto</a>
-             </figure>
-          </div>
-
           <div className="box">
             <img src={survivalSland} alt="imagem do site survival sland" />
             <figure className='box-infor'>
@@ -66,7 +40,29 @@ export const Projects = ({projectPage}) => {
              </figure>
           </div>
 
+          <div className="box">
+            <img src={TaskList} alt="" />
+            <figure className='box-infor'>
+                <nav>
+                <h2 className='infor-title'>TaskList </h2>
+                <p className='infor-text'> <span className='span-infor'>Uma lista de tarefas pra me organizar melhor</span> </p>
+                </nav>
 
+                <a className='btn-to-project' href='https://gust17x.github.io/task-list/'>Ver projeto</a>
+             </figure>
+          </div>
+
+          <div className="box">
+            <img src={SiteRelogio} alt="Imagem do site relogio" />
+            <figure className='box-infor'>
+                <nav>
+                <h2 className='infor-title'>Relogio</h2>
+                <p className='infor-text'><span className='span-infor'>Um relogio in live</span></p>
+                </nav>
+
+                <a className='btn-to-project' href='https://gust17x.github.io/Relogio/'>Ver projeto</a>
+             </figure>
+          </div>
 
           <div className="box">
             <img src={EnvEmail} alt="Imagem do site .env" />
@@ -81,14 +77,13 @@ export const Projects = ({projectPage}) => {
           </div>
 
           <div className="box">
-            <img src={provaDev} alt="Imagem do site .env" />
             <figure className='box-infor'>
                 <nav>
-                <h2 className='infor-title'>Prova-dev</h2>
-                <p className='infor-text'><span className='span-infor'>Uma prova com questões sobre programação</span></p>
+                <h2 className='infor-title'></h2>
+                <p className='infor-text'><span className='span-infor'>Site que te envia um email com as informaçõe que vc preencher</span></p>
                 </nav>
 
-                <a className='btn-to-project' href='https://gust17x.github.io/ProvaDev/'>Ver projeto</a>
+                <a className='btn-to-project' href='#'>Ver projeto</a>
              </figure>
           </div>
 
